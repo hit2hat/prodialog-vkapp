322ba59613c7ca2cd0fc6d3f7a0c3df6a3df32dc
hit2hat <hit2hat@users.noreply.github.com>
2019-05-31 19:44:57 +0000

    Deploying to gh-pages - 19:44:57

---
+++ precache-manifest.91c6ac544bbdb07e57259b2756c141fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "71902e4a6c4fea351c234a81e38fce43",
+    "url": "/prodialog-vkapp/static/media/logo.71902e4a.svg"
+  },
+  {
+    "revision": "a1b5de1a338b2cdb2860",
+    "url": "/prodialog-vkapp/static/js/runtime~main.a77532d9.js"
+  },
+  {
+    "revision": "a09874b6e7cbe85bb028",
+    "url": "/prodialog-vkapp/static/js/main.652b2144.chunk.js"
+  },
+  {
+    "revision": "7430ba2464e42eb8c875",
+    "url": "/prodialog-vkapp/static/js/2.5337a853.chunk.js"
+  },
+  {
+    "revision": "7430ba2464e42eb8c875",
+    "url": "/prodialog-vkapp/static/css/2.e5a19977.chunk.css"
+  },
+  {
+    "revision": "abbd38872115338a810cbab06d48554b",
+    "url": "/prodialog-vkapp/index.html"
+  }
+];
